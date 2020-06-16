0b96d5eefecd8bd158a05e78ec4d8eddebaf0ebc
AnyThingElse <57551358+AnyThing-X@users.noreply.github.com>
2020-06-16 05:26:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const puppeteer = require('puppeteer');
-let results = JSON.parse(`{"restricted": false,"suspended": false,"notfound": false,"working": false}`);
-const express = require("express");
+const express = require('express'), puppeteer = require('puppeteer'), path = require('path');
+var results = JSON.parse(`{"restricted": false,"suspended": false,"notfound": false,"working": false}`);
 var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/uu/', (req, res) => res.render('pages/index'))
+
 app.get('/:user', async function (req, res) {
-  let browser = await puppeteer.launch({ headless: true, devtools: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-  let page = await browser.newPage();
-  await page.goto(`https://twitter.com/${req['params']['user']}`, { waitUntil: 'networkidle0' });
-  try {
-    let [ele1] = await page.$x(process.env.XPATH1);
-    let rawTxt1 = await (await ele1.getProperty('textContent')).jsonValue();
-    if (rawTxt1 == 'Caution: This account is temporarily restricted') { results['restricted'] = true; }
-    else if (rawTxt1 == 'Account suspended') { results['suspended'] = true; }
-    else if (rawTxt1 == 'This account doesn’t exist') { results['notfound'] = true; }
-    await browser.close();
-  } catch (e) {
+    let browser = await puppeteer.launch({ headless: true, devtools: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    let page = await browser.newPage();
+    await page.goto(`https://twitter.com/${req['params']['user']}`, { waitUntil: 'networkidle0' });
     try {
-      let [ele2] = await page.$x(process.env.XPATH2);
-      let rawTxt2 = await (await ele2.getProperty('textContent')).jsonValue();
-      if (rawTxt2 == 'Tweets') { results['working'] = true; await browser.close(); }
+        let [ele1] = await page.$x(process.env.XPATH1);
+        let rawTxt1 = await (await ele1.getProperty('textContent')).jsonValue();
+        if (rawTxt1 == 'Caution: This account is temporarily restricted') { results['restricted'] = true; }
+        else if (rawTxt1 == 'Account suspended') { results['suspended'] = true; }
+        else if (rawTxt1 == 'This account doesn’t exist') { results['notfound'] = true; }
+        await browser.close();
     } catch (e) {
-      results['notfound'] = true; await browser.close();
+        try {
+            let [ele2] = await page.$x(process.env.XPATH2);
+            let rawTxt2 = await (await ele2.getProperty('textContent')).jsonValue();
+            if (rawTxt2 == 'Tweets') { results['working'] = true; await browser.close(); }
+        } catch (e) {
+            results['notfound'] = true; await browser.close();
+        }
     }
-  }
-  res.type('json').send(JSON.stringify(results, null, 2));
+    res.type('json').send(JSON.stringify(results, null, 2));
 });
-app.listen(process.env.PORT||8080);
+app.listen(process.env.PORT || 5000);
