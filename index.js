febd56b70ca7359cfb5485880a84079cf6fa835f
AnyThingElse <57551358+AnyThing-X@users.noreply.github.com>
2020-06-19 23:56:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
-
-var http = require('http'),url = require('url');
-const puppeteer = require('puppeteer');
-
+const http = require('http'), url = require('url'), puppeteer = require('puppeteer');
 http.createServer(async function (req, res) {
-  var query = url.parse(req.url, true).query;
-  try {
-  let browser = await puppeteer.launch({ headless: true,args: ['--no-sandbox'] });
-  let page = await browser.newPage();
-  await page.goto(`https://twitter.com/${query.user}`, { waitUntil: 'networkidle0' });
-
-    let [ele1] = await page.$x(process.env.XPATH1);
-    let rawTxt1 = await (await ele1.getProperty('textContent')).jsonValue();
-    
-    if (rawTxt1 == 'Caution: This account is temporarily restricted') {
-    res.end('restricted');
-    }
-    else if (rawTxt1 == 'Account suspended') {
-      res.end('suspended');
-    }
-    else if (rawTxt1 == 'This account doesn’t exist') {
-      res.end('notfound');
-    }
-    await browser.close();
-  } catch (e) {
+    let query = url.parse(req.url, true).query;
+    let browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+    let page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+    await page.goto(`https://twitter.com/${query.user}`);
     try {
-        res.end('working');
+        let em = await page.waitForXPath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div/div/div/div[2]/div[1]/span', { timeout: 2500, visible: true })
+        let raw = await (await em.getProperty('textContent')).jsonValue()
+
+        if (raw == 'Caution: This account is temporarily restricted') {
+            res.end('restricted');
+        }
+        else if (raw == 'Account suspended') {
+            res.end('suspended');
+        }
+        else if (raw == 'This account doesn’t exist') {
+            res.end('notfound');
+        }
         await browser.close();
     } catch (e) {
-      res.end('notfound');
-      await browser.close();
+        res.end('working');
+        await browser.close();
     }
-    
-  }
 }).listen(process.env.PORT || 8080);
-
