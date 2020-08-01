cda01abefbb2a5f09ade65a90d1897c146adc07e
AnyThingElse <57551358+AnyThing-X@users.noreply.github.com>
2020-08-02 01:37:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const ytScraper = require("yt-scraper")
-const http = require('http'), url = require('url')
+var ytScraper = require("yt-scraper")
+var http = require('http'), url = require('url')
 http.createServer(async function (req, res) {
     let query = url.parse(req.url, true).query;
-    if (query.user) {
-        ytScraper.videoInfo(query.user)
+    if (query.id) {
+        ytScraper.videoInfo(query.id)
         .then(data => {
             res.end(`Title : ${data.title}\nDescription : ${data.description}\n${data.url}`)
         });
